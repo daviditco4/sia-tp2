93eff49d298532330872953d36ad17e850b3646a
David Itcovici <ditcovici@MacBook-Air-de-David.local>
2024-09-09 15:50:04 -0300

    Create EVE performance calculation

---
+++ sia_tp2/Character.py
@@ -0,0 +1,10 @@
+class Character:
+    def __init__(self, char_class, strength_points, dexterity_points, intelligence_points, vigor_points,
+                 constitution_points, height):
+        self.char_class = char_class
+        self.strength_points = strength_points
+        self.dexterity_points = dexterity_points
+        self.intelligence_points = intelligence_points
+        self.vigor_points = vigor_points
+        self.constitution_points = constitution_points
+        self.height = height
